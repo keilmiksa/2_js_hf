9db586634128cb4a02631f787d7be31252b4830d
keilmiksa <keil.miksa.6a@gmail.com>
2025-04-02 20:18:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,79 @@
 $(document).ready(function() {
-    let currentIndex = 0;
+    let currentIndex = 0; // A jelenleg kiválasztott kép indexét tárolja
     const images = $('.gallery-image img').map(function() {
+        // Az összes képfájl elérési útját összegyűjti egy tömbbe
         return $(this).attr('src');
     }).get();
 
+    // Frissíti a modal ablakban látható képet az aktuális index alapján
     function updateModalImage() {
         $('#modal-image').attr('src', images[currentIndex]);
     }
 
+    // Ha egy képre kattintunk, megnyitja a modal ablakot és beállítja a nagyított képet
     $('.gallery-image img').on('click', function() {
-        currentIndex = images.indexOf($(this).attr('src'));
-        updateModalImage();
-        $('#modal').removeClass('hidden');
+        currentIndex = images.indexOf($(this).attr('src')); // Meghatározza a kattintott kép indexét
+        updateModalImage(); // Frissíti a modal ablakban a képet
+        $('#modal').removeClass('hidden'); // Megjeleníti a modal ablakot
     });
 
+    // Előző kép megjelenítése (balra nyíl)
     $('#prev-btn').on('click', function() {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        updateModalImage();
+        currentIndex = (currentIndex - 1 + images.length) % images.length; // Az előző kép indexének kiszámítása
+        updateModalImage(); // Frissíti a modal ablakban a képet
     });
 
+    // Következő kép megjelenítése (jobbra nyíl)
     $('#next-btn').on('click', function() {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateModalImage();
+        currentIndex = (currentIndex + 1) % images.length; // A következő kép indexének kiszámítása
+        updateModalImage(); // Frissíti a modal ablakban a képet
     });
 
+    // Modal bezárása a "Bezárás" gomb segítségével
     $('#close-btn').on('click', function() {
-        $('#modal').addClass('hidden');
+        $('#modal').addClass('hidden'); // Elrejti a modal ablakot
     });
 
+    // Képek előtöltése, hogy gyorsabban jelenjenek meg
     function preloadImages(imageArray) {
         imageArray.forEach(function(src) {
             const img = new Image();
-            img.src = src;
+            img.src = src; // Minden kép forrását betölti a böngészőbe
         });
     }
     
-    $(document).ready(function() {
-        const imageSources = $('.gallery-image img').map(function() {
-            return $(this).attr('src');
-        }).get();
-        preloadImages(imageSources);
-    });
+    // Az előtöltési funkció meghívása az összes galéria kép forrására
+    const imageSources = $('.gallery-image img').map(function() {
+        return $(this).attr('src');
+    }).get();
+    preloadImages(imageSources); // Előtöltés elindítása
 
-let startX = 0;
-let endX = 0;
+    let startX = 0; // Az érintés kezdő pozíciójának tárolása
+    let endX = 0;   // Az érintés végpozíciójának tárolása
 
-    // Touch start esemény (ujj érintése a képernyőhöz)
+    // Touch start esemény: az ujj érintése a képernyőhöz
     $('#modal').on('touchstart', function(e) {
-        startX = e.originalEvent.touches[0].clientX; // Az érintés kezdő pozíciója
+        startX = e.originalEvent.touches[0].clientX; // Érintés kezdő vízszintes koordinátája
     });
 
-    // Touch end esemény (ujj felemelése a képernyőről)
+    // Touch end esemény: az ujj felemelése a képernyőről
     $('#modal').on('touchend', function(e) {
-        endX = e.originalEvent.changedTouches[0].clientX; // Az érintés végpozíciója
-        handleSwipe();
+        endX = e.originalEvent.changedTouches[0].clientX; // Érintés vége vízszintes koordinátája
+        handleSwipe(); // Meghívja a húzáskezelő logikát
     });
 
+    // Húzáskezelő logika
     function handleSwipe() {
-        const swipeDistance = startX - endX;
+        const swipeDistance = startX - endX; // Kiszámolja a húzás távolságát és irányát
 
         if (swipeDistance > 50) {
             // Balra húzás: következő kép
-            currentIndex = (currentIndex + 1) % images.length;
-            updateModalImage();
+            currentIndex = (currentIndex + 1) % images.length; // Következő kép indexének kiszámítása
+            updateModalImage(); // Frissíti a modal ablakban a képet
         } else if (swipeDistance < -50) {
             // Jobbra húzás: előző kép
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
-            updateModalImage();
+            currentIndex = (currentIndex - 1 + images.length) % images.length; // Előző kép indexének kiszámítása
+            updateModalImage(); // Frissíti a modal ablakban a képet
         }
     }
-
 });
-
